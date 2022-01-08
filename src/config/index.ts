679e5613f05859692c72152d3912d5ad172736a5
Ricky Almeida <ricky.almeida.dev@gmail.com>
2022-01-08 18:35:05 -0300

    setup react-native-dotenv

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+import { OPEN_WEATHER_KEY } from 'react-native-dotenv';
+
+const Config = {
+  OPEN_WEATHER_KEY: OPEN_WEATHER_KEY,
+};
+
+export default Config;
---
+++ src/types/env.d.ts
@@ -0,0 +1,3 @@
+declare module 'react-native-dotenv' {
+  export const OPEN_WEATHER_KEY: string;
+}

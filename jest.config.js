b81415bf48dc62debe88f2d95acb328341605ac0
Ricky Almeida <ricky.almeida.dev@gmail.com>
2022-01-08 18:24:45 -0300

    add jest and testing-library configs

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/$1',
+    '^@testing-library$': '<rootDir>/testing-library.config.ts',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+};
